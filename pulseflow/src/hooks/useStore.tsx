de9eb3dc4b26e6f13bdfdf9dea5b8459cbdbc0f7
huddavishal-610 <huddavishal610@gmail.com>
2025-08-25 00:30:30 +0530

    Delete pulseflow directory

--- pulseflow/src/hooks/useStore.tsx
+++
@@ -1,8 +0,0 @@
-
-import { useSyncExternalStore } from 'react'
-import { getState, subscribe, setState, type AppState } from '../state/store'
-
-export function useStore<T>(selector: (s:AppState)=>T){
-  return useSyncExternalStore(subscribe, ()=>selector(getState()), ()=>selector(getState()))
-}
-export const setAppState = setState
